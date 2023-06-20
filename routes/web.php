5be95295c3fa320bcc164f3e616dbec6b2905b80
nabin  paneru <nabinpaneru141@gmail.com>
2023-06-20 12:40:12 +0545

    customize registration,login and forgot-password form

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,6 +18,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/admin/logout',[AdminController::class,'destroy'])->name('admin.logout');
 
 Route::get('/dashboard', function () {
     return view('admin.index');
